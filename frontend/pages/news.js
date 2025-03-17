c3d88b167e66abab9badb7cb45162ec65ac00edf
Dejan <radovanovic.dejan2804@gmail.com>
2025-03-17 20:30:59 +0100

    Blogposts heading;

--- frontend/pages/news.js
+++ frontend/pages/news.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
+import Image from 'next/image';
+import porscheImg from "../../photos/porsche.png";
 
 export default function News() {
   const [posts, setPosts] = useState([]);
@@ -33,22 +35,33 @@ export default function News() {
   }
 
   return (
-    <>
-      <h1>News</h1>
-      {posts.map((post) => (
-        <div
-          key={post.id} className = 'drr-blogpost-container'>
-          <h2>{post.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
-          <p>
-            Created on: {new Date(post.created_at).toLocaleDateString()} | Updated on:{' '}
-            {new Date(post.updated_at).toLocaleDateString()}
-          </p>
-          <Link href={`/news/${post.slug}`}>
-            Read More
-          </Link>
-        </div>
-      ))}
-    </>
+      <>
+          <h1 className="drr-breadcrump">Home / News</h1>
+          <div class="drr-blog-header">
+              <div class="left">
+                  <h2>Blog.</h2>
+                  <p>Sveobuhvatan prikaz dešavanja, analiza i inovacija u automobilskoj industriji.</p>
+              </div>
+              <div class="right">
+                <img src={porscheImg.src} alt="Porsche"/>
+              </div>
+          </div>
+          {/*TEMP*/}
+          <div style={{marginTop: 200 + 'vh'}}></div>
+          {posts.map((post) => (
+              <div
+                  key={post.id} className='drr-blogpost-container'>
+                  <h2>{post.title}</h2>
+                  <div dangerouslySetInnerHTML={{__html: post.content}}/>
+                  <p>
+                      Created on: {new Date(post.created_at).toLocaleDateString()} | Updated on:{' '}
+                      {new Date(post.updated_at).toLocaleDateString()}
+                  </p>
+                  <Link href={`/news/${post.slug}`}>
+                      Read More
+                  </Link>
+              </div>
+          ))}
+      </>
   );
 }
