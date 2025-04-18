b48d7d7561e35e4757e527c9e253a051c1d74c99
Dejan <radovanovic.dejan2804@gmail.com>
2025-04-18 13:28:54 +0200

    Added page tracking;

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,41 @@
 // frontend/pages/_app.js
 import { SessionProvider } from "next-auth/react";
-import BaseTemplate from "../components/BaseTemplate"; // import your layout
+import BaseTemplate from "../components/BaseTemplate";
+import Script from "next/script";
+
 import '../styles/globals.css';
 import '../styles/resetstyle.css';
 import '../styles/sidepanel.css';
 import '../styles/rightpanel.css';
 import '../styles/blogposts.css';
-import '../styles/downpanel.css'
+import '../styles/downpanel.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <BaseTemplate>
-        <Component {...pageProps} />
-      </BaseTemplate>
-    </SessionProvider>
+    <>
+      {/* Load gtag.js */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-5G5Z9JDLVV"
+        strategy="afterInteractive"
+      />
+      {/* Initialize Google Analytics */}
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-5G5Z9JDLVV', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <SessionProvider session={session}>
+        <BaseTemplate>
+          <Component {...pageProps} />
+        </BaseTemplate>
+      </SessionProvider>
+    </>
   );
 }
 
