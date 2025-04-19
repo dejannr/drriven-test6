4c948a997aae4f85f36e2476aefa3f2b8631a59a
Dejan <radovanovic.dejan2804@gmail.com>
2025-04-19 09:09:36 +0200

    Added notifications;

--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -3,32 +3,52 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Link from "next/link";
+import Notification from "../components/Notification";
 
 export default function Register() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [notification, setNotification] = useState(null);
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/register`, {
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL_FRONT}/api/register`, {
         username,
         email,
         password,
       });
-      alert(response.data.message);
-      router.push("/login");
+      setNotification({ type: "success", message: response.data.message, duration: 3000 });
+
+      setTimeout(() => {
+        router.push("/login");
+      }, 3000);
     } catch (error) {
-      console.error("Registration error:", error.response?.data || error.message);
-      alert("Registration failed: " + JSON.stringify(error.response?.data));
+      const errMap = error.response?.data.error ?? {};
+      const errorMsg = Object.values(errMap)[0]  //
+      console.error("Registration error:", errorMsg[0]);
+      setNotification({
+        type: "error",
+        message: "Registration failed: " + errorMsg[0],
+        duration: 4000,
+      });
     }
   };
 
   return (
     <>
+      {notification && (
+        <Notification
+          type={notification.type}
+          message={notification.message}
+          duration={notification.duration}
+          onClose={() => setNotification(null)}
+        />
+      )}
+
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
         <div>
